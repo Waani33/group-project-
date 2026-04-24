da531a4a3b9d9d4af42a3298d835f5ac508f90c5
Reese Lawrence <reesel2006@gmail.com>
2026-04-24 13:05:55 -0500

    Added code for ver1

--- coffee.py
+++ coffee.py
@@ -1,1 +1,12 @@
-print('coffee')
+import serial
+import time
+
+arduino = serial.Serial('COM3', 9600, timeout=1)
+time.sleep(2)  # let Arduino reset
+
+arduino.write(b'PRESS\n')
+
+response = arduino.readline().decode().strip()
+print(response)
+
+arduino.close()
